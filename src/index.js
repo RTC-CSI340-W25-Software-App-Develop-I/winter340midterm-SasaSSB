f1116f3a0ec5fa6f7ff476b6100018dc423fbb65
SBraynen <sssbraynen@outlook.com>
2025-02-11 18:47:24 -0800

    SBraynen_winter340midterm

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-//Do not change //////////////////////////////////
-import { calculateStarAverage } from "../src/logic.js";
+//Do not change //////////////////////////////////s
 
 const reviews = [
   {
@@ -22,6 +21,59 @@ const reviews = [
       "The coffee was great but the employees didn't let me stay past closing! ): Worst experience ever.",
   },
 ];
+
+const form = document.querySelector("form");
+
+//console.log(form);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = {
+    // information for user review
+    image: e.target.image.value,
+    username: e.target.username.value,
+    star: e.target.star.value,
+    review: e.target.review.value,
+  };
+
+  renderConfirmation(formData);
+});
+
+const renderConfirmation = (formData) => {
+  const imageP = document.createElement("img");
+  //formData.image to get the image value
+  if (formData.image === "./images/rose.png") {
+    imageP.src = "./images/rose.png";
+  } else if (formData.image === "./images/avatar2.png") {
+    imageP.src = "./images/avatar2.png";
+  } else if (formData.image === "./images/avatar1.png") {
+    imageP.src = "./images/avatar1.png";
+  } else {
+    imageP.src = "https://cdn-icons-png.flaticon.com/512/149/149071.png"; //Userimage If no image from images is added
+  }
+  //bug showing only last image if using in list
+  //bug fixed used a else if statment loop instead of list
+
+  document.body.appendChild(imageP);
+
+  const usernameP = document.createElement("p");
+  usernameP.textContent = formData.username;
+  document.body.appendChild(usernameP);
+
+  const starP = document.createElement("p");
+  starP.textContent = formData.star;
+  document.body.appendChild(starP);
+
+  const reviewP = document.createElement("p");
+  reviewP.textContent = formData.review;
+  document.body.appendChild(reviewP);
+
+  const section = document.querySelector("section");
+  section.innerHTML += ""; //+= adds review after review
+
+  section.append(imageP, usernameP, starP, reviewP);
+};
+
 /////////////////////////////////////////////////////////////////////
 
 //1. Append the reviews to the DOM
--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-import { assert } from "chai";
+import { assert, expect } from "chai";
 
 import { calculateStarAverage } from "../src/logic.js";
 
@@ -23,3 +23,19 @@ const mockReviews = [
       "The coffee was great but the employees didn't let me stay past closing! ): Worst experience ever.",
   },
 ];
+
+describe("calculateStarAverage Function", () => {
+  it('stars average (5+3+1)/3 should equal "3"', () => {
+    const result = calculateStarAverage([
+      { star: 5 },
+      { star: 3 },
+      { star: 1 },
+    ]);
+    assert.equal(result, 3); // 9/3=3
+  });
+
+  it('5 single review should equal"5"', () => {
+    const result = calculateStarAverage([{ star: 5 }]);
+    assert.equal(result, 5); // 5/1=5
+  });
+});
